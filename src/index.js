eafed814ddebcf4c6c576b4d303a9036603ba840
Max Wheeler <max@makenosound.com>
2016-05-19 15:32:04 +1000

    Simplify the main export since we no longer need to curry it.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
 import processBlocks from './processor'
-import compiler from './compiler'
-
-const defaults = {
-  allowEmptyTags: false
-}
 
 /**
  * Exporter
  *
- * Curry the converter function with the set of (compiled) renderers and
- * configuration.
- *
- * @param  {Object} renderers A set of compatible renderers
- * @param  {Object} config Configuration
- * @return {Function} Converter function
+ * @param  {EditorState} editorState Draft JS EditorState object
+ * @return {Array} An abstract syntax tree representing the draft-js editorState
  */
-function exporter (renderers, config) {
-  config = Object.assign({}, defaults, config)
-  const renderer = compiler(renderers, config)
-
-  /**
-   * Converter
-   *
-   * Take an EditorState and convert it using our renderers
-   *
-   * @param  {EditorState} editorState An EditorState object from draft-js
-   * @return {Array} Converted data
-   */
-  return function convert (editorState) {
-    // Retrieve the content
-    const content = editorState.getCurrentContent()
-    const blocks = content.getBlocksAsArray()
-    // Convert to an abstract syntax tree
-    const processedBlocks = processBlocks(blocks, [])
-    // Render the content
-    return renderer(processedBlocks)
-  }
+function exporter (editorState) {
+  // Retrieve the content
+  const content = editorState.getCurrentContent()
+  const blocks = content.getBlocksAsArray()
+  // Convert to an abstract syntax tree
+  return processBlocks(blocks)
 }
 
 export default exporter
