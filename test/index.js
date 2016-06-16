651c807bea12d97dad6f4965ab40481c8f2130dd
Max Wheeler <max@makenosound.com>
2016-06-16 16:10:15 +1000

    Add basic tests.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,46 @@
 import test from 'tape'
-import isFunction from 'is-function'
+import {Map} from 'immutable'
+import {
+  convertFromRaw,
+  convertToRaw,
+  EditorState,
+} from 'draft-js'
 import exporter from '../src'
+import content from './fixtures/content'
+import contentExported from './fixtures/content-exported'
+import contentExportedEntity from './fixtures/content-exported-entity'
+
+test('it should export data', (nest) => {
+  const contentState = convertFromRaw(content)
+  const editorState = EditorState.createWithContent(contentState)
+
+  nest.test('... to an abstract syntax tree', (assert) => {
+    const actual = exporter(editorState)
+    const expected = contentExported
+    assert.deepEqual(actual, expected, 'exported data is an array')
+    assert.end()
+  })
+
+  nest.test('... with entity modifications', (assert) => {
+    // Simple modifiers to make URLs protocol-less
+    const options = {
+      entityModifiers: {
+        'LINK': (data) => {
+          let copy = Object.assign({}, data)
+          copy.url = copy.url.replace(/^https?:/, '')
+          return copy
+        },
+        'image': (data) => {
+          let copy = Object.assign({}, data)
+          copy.src = copy.src.replace(/^https?:/, '')
+          return copy
+        },
+      },
+    }
+    const actual = exporter(editorState, options)
+    const expected = contentExportedEntity
+    assert.deepEqual(actual, expected, 'exported entity data is modified')
+    assert.end()
+  })
+
+})
